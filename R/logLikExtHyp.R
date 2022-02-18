946bb47afe1d543b1ecfb7066c7567c10736b835
kpmainali <kpmainali@gmail.com>
2022-02-18 13:43:53 -0500

    logLikExtHyp edited

--- R/logLikExtHyp.R
+++ R/logLikExtHyp.R
@@ -1,21 +1,24 @@
-#' Repeat as stand-alone calculations of part of ML.Alpha()
+#' log of Extended Hypergeometric Likelihiood at (X, mA,mB,N, alpha)
 #'
-#' This function calculates ... to be added
+#' This function calculates the logarithm of the Extended Hypergeometric likelihood at specified x and alpha, with marginal totals mA, mB, N fixed.
 #'
-#' @details   to be added
+#' @details   This is simply the logarithm of the Extended Hypergeometric (Harkness 1965) or Fisher noncentral Hypergeometric, as calculated by the R package BiasedUrn. The formula is  log(pFNCHypergeo(x,mA,N-mA,mB,exp(alpha))
 #'
 #' @param x integer co-occurrence count that should properly fall within the closed interval \[max(0,mA+mB-N), min(mA,mB)\]
 #' @param marg a 3-entry integer vector (mA,mB,N) consisting of the first row and column totals and the table total for a 2x2 contingency table
+#' @param alpha a real number, the log odds ratio or affinity parameter for the 2x2 contingency table
 #'
-#' @return to be added
+#' @return scalar loglikelihood value
 #'
 #' @author Eric Slud
 #'
 #' @references
-#' to be added
+#' Fog, A. (2015), BiasedUrn: Biased Urn Model Distributions. R package version 1.07.
+#'
+#' Harkness, W. (1965), “Properties of the extended hypergeometric distribution“, Annals of Mathematical Statistics, 36, 938-945.
 #'
 #' @example
-#' to be added
+#' examples/logLikExtHyp_example.R
 #'
 #' @export
 
---
+++ examples/logLikExtHyp_example.R
@@ -0,0 +1,2 @@
+require(BiasedUrn)
+c(logLikExtHyp(30,c(50,80,120),1),  log(dFNCHypergeo(30,50,70,80,exp(1))))
