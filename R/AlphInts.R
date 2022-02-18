089856136db17b94d4d5cf6613157720c837c2ee
kpmainali <kpmainali@gmail.com>
2022-02-18 14:59:16 -0500

    example linked

--- R/AlphInts.R
+++ R/AlphInts.R
@@ -61,7 +61,7 @@
 #' Harkness, W. (1965), “Properties of the extended hypergeometric distribution“, Annals of Mathematical Statistics, 36, 938-945.
 #'
 #' @example
-#' to be added
+#' examples/AlphInts_example.R
 #'
 #' @export
 
